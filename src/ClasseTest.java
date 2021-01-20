c89aa6c32e55f3710d049493b6634a428a4031f4
CotRa2032409 <2032409@etu.cegepjonquiere.ca>
2021-01-20 14:44:41 -0500

    Création de la ClasseTest

--- src/ClasseTest.java
+++ src/ClasseTest.java
@@ -1,8 +1,8 @@
 public class ClasseTest {
 
     public static void main(String[] args) {
-        int x = 2;
         System.out.println(42);
         System.out.println("Allo");
+        int y = (int) Math.pow(2,2);
     }
 }
