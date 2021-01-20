887b84339b9daaeae54ffbc4a0fc3ec8f47e017f
CotRa2032409 <2032409@etu.cegepjonquiere.ca>
2021-01-20 14:50:29 -0500

    Test

--- src/ClasseTest.java
+++ src/ClasseTest.java
@@ -3,7 +3,8 @@ public class ClasseTest {
     public static void main(String[] args) {
         System.out.println(42);
         System.out.println("Allo");
-        int y = (int) Math.pow(2,2);
+        int y = (int) Math.pow(2, 2);
         int x = 2;
+        int z = -2;
     }
 }
